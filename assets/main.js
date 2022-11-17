712d052956f59b47b68bda392011031ec2db5ccb
Alexander Gosal <96303611+Alxx-0212@users.noreply.github.com>
2022-11-17 21:23:55 +0800

    Menghapus toggle__button dan menambah fitur add dan del pada halaman sales

--- assets/main.js
+++ assets/main.js
@@ -1,27 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var sidebar = document.querySelector(".sidebar");
-    var toggle_button = document.querySelector(".toggle-button");
+	var sidebar = document.querySelector(".sidebar");
 
-    toggle_button.addEventListener("click", () => {
-        toggle_button.classList.toggle("__active");
-        sidebar.classList.toggle("__active");
-        let title_elem = document.querySelectorAll(".title");
-        for (let i = 0; i < title_elem.length; i++) {
-            title_elem[i].classList.toggle("__active");
-        }
-    });
+	sidebar.addEventListener("mouseover", () => {
+		sidebar.classList.toggle("__active");
+		let title_elem = document.querySelectorAll(".title");
+		for (let i = 0; i < title_elem.length; i++) {
+			title_elem[i].classList.toggle("__active");
+		}
+	});
+	sidebar.addEventListener("mouseout", () => {
+		sidebar.classList.toggle("__active");
+		let title_elem = document.querySelectorAll(".title");
+		for (let i = 0; i < title_elem.length; i++) {
+			title_elem[i].classList.toggle("__active");
+		}
+	});
 
-    var list = document.querySelectorAll(".sidebar-list");
+	var list = document.querySelectorAll(".sidebar-list");
 
-    for (let i = 0; i < list.length; i++) {
-        list[i].addEventListener("click", function () {
-            let x = 0;
-            while (x < list.length) {
-                list[x++].className = "sidebar-list";
-            }
-            list[i].className = "sidebar-list __active";
-        });
-    }
+	for (let i = 0; i < list.length; i++) {
+		list[i].addEventListener("click", function () {
+			let x = 0;
+			while (x < list.length) {
+				list[x++].className = "sidebar-list";
+			}
+			list[i].className = "sidebar-list __active";
+		});
+	}
 })
 
 const labels = ["January", "February", "March", "April", "May", "June"];
