991acd74376ce10fe678dee03671a47ee2a859cf
raynaldysuchiady <rsuchiady@gmail.com>
2022-12-04 16:49:21 +0800

    Memperbaiki icon cart

--- resources/views/supply.blade.php
+++ resources/views/supply.blade.php
@@ -18,48 +18,58 @@
                 <div class="Boxes__title">Total Order</div>
                 <div class="Boxes__number">40,876</div>
                 <div class="Boxes__icon">
-                  <i class='bx bx-up-arrow-alt'></i>
+                  <ion-icon name="arrow-up-outline"></ion-icon>
                   <span class="Boxes__text">Up from yesterday</span>
                 </div>
               </div>
-              <i class='bx bx-cart-alt cart'></i>
+              <div class="Boxes__iconCart">
+              <ion-icon name="cart-outline"></ion-icon>
+              </div>
             </div>
             <div class="Boxes__box">
               <div class="Boxes__RightSide">
                 <div class="Boxes__title">Total Sales</div>
                 <div class="Boxes__number">38,876</div>
                 <div class="Boxes__icon">
-                  <i class='bx bx-up-arrow-alt'></i>
+                  <ion-icon name="arrow-up-outline"></ion-icon>
                   <span class="Boxes__text">Up from yesterday</span>
                 </div>
               </div>
-              <i class='bx bxs-cart-add cart two' ></i>
+              <div class="Boxes__iconCart2">
+              <ion-icon name="cart-outline"></ion-icon>
+              </div>
             </div>
             <div class="Boxes__box">
               <div class="Boxes__RightSide">
                 <div class="Boxes__title">Total Profit</div>
                 <div class="Boxes__number">$12,876</div>
                 <div class="Boxes__icon">
-                  <i class='bx bx-up-arrow-alt'></i>
+                  <ion-icon name="arrow-up-outline"></ion-icon>
                   <span class="Boxes__text">Up from yesterday</span>
                 </div>
               </div>
-              <i class='bx bx-cart cart three' ></i>
+              <div class="Boxes__iconCart3">
+              <ion-icon name="cart-outline"></ion-icon>
+              </div>
             </div>
             <div class="Boxes__box">
               <div class="Boxes__RightSide">
                 <div class="Boxes__title">Total Return</div>
                 <div class="Boxes__number">11,086</div>
                 <div class="Boxes__icon">
-                  <i class='bx bx-down-arrow-alt down'></i>
+                  <div class="Boxes__iconDown">
+                  <ion-icon name="arrow-down-outline"></ion-icon>
+                  </div>
                   <span class="Boxes__text">Down From Today</span>
                 </div>
               </div>
-              <i class='bx bxs-cart-download cart four' ></i>
+              <div class="Boxes__iconCart4">
+              <ion-icon name="cart-outline"></ion-icon>
+              </div>
             </div>
           </div>
         </section>
-
+        <span>
         <div class="SupplyTable">
             <table>
                 <thead>
@@ -96,5 +106,8 @@
                 </tbody>
             </table>
         </div>
+        </span>
+        <span class="TopProduct">sdsdsds
+        </span>
     </main>
 @endsection
