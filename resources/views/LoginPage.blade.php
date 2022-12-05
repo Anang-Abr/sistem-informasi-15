c69f726b533e5aec96c5b920ebe8d23fb906a018
Alexander Gosal <96303611+Alxx-0212@users.noreply.github.com>
2022-12-05 20:41:02 +0800

    Memperbaiki beberapa variabel untuk halaman customer, login dan halaman addCustomer.

--- resources/views/LoginPage.blade.php
+++ resources/views/LoginPage.blade.php
@@ -1,29 +1,29 @@
 @extends('components.main')
 @section('content')
-<div class="login-container">
-    <div class="login-wrapper">
-         <div class="login-title">
+<div class="login__body">
+    <div class="login__container">
+         <div class="login__title">
             Welcome !
          </div>
-         <form class="login-form" action="#">
-            <div class="login-input-field">
+         <form class="login__form" action="#">
+            <div class="login__input-field">
                <input type="text" required>
                <label>Email Address</label>
             </div>
-            <div class="login-input-field">
+            <div class="login__input-field">
                <input type="password" required>
                <label>Password</label>
             </div>
-            <div class="login-content">
-               <div class="login-checkbox">
+            <div class="login__content">
+               <div class="login__checkbox">
                   <input type="checkbox" id="remember-me">
                   <label for="remember-me">Remember me</label>
                </div>
-               <div class="login-pass-link">
+               <div class="login__pass-link">
                   <a href="#">Forgot password?</a>
                </div>
             </div>
-            <div class="login-input-field" id="login-submit">
+            <div class="login__input-field" id="login__submit">
                <input type="submit" value="Login">
             </div>
          </form>
