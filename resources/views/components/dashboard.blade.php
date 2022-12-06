583dba7c1d0b525344dfcf01873468b9f0626828
Anang-Abr <ananabr2610@gmail.com>
2022-12-06 10:22:13 +0800

    memperbaiki tampilan halaman dashboard

---
+++ resources/views/components/dashboard.blade.php
@@ -0,0 +1,58 @@
+<div class="HomeContent">
+          <div class="Boxes">
+            <div class="Boxes__box">
+              <div class="Boxes__RightSide">
+                <div class="Boxes__title">Total Order</div>
+                <div class="Boxes__number">40,876</div>
+                <div class="Boxes__icon">
+                  <ion-icon name="arrow-up-outline"></ion-icon>
+                  <span class="Boxes__text">Up from yesterday</span>
+                </div>
+              </div>
+              <div class="Boxes__iconCart">
+              <ion-icon name="cart-outline"></ion-icon>
+              </div>
+            </div>
+            <div class="Boxes__box">
+              <div class="Boxes__RightSide">
+                <div class="Boxes__title">Total Sales</div>
+                <div class="Boxes__number">38,876</div>
+                <div class="Boxes__icon">
+                  <ion-icon name="arrow-up-outline"></ion-icon>
+                  <span class="Boxes__text">Up from 1 month ago</span>
+                </div>
+              </div>
+              <div class="Boxes__iconSales">
+              <ion-icon name="bag-check-outline"></ion-icon>
+              </div>
+            </div>
+            <div class="Boxes__box">
+              <div class="Boxes__RightSide">
+                <div class="Boxes__title">Total Income</div>
+                <div class="Boxes__number">$12,876</div>
+                <div class="Boxes__icon">
+                  <ion-icon name="arrow-up-outline"></ion-icon>
+                  <span class="Boxes__text">Up from yesterday</span>
+                </div>
+              </div>
+              <div class="Boxes__iconProfit">
+              <ion-icon name="cash-outline"></ion-icon>
+              </div>
+            </div>
+            <div class="Boxes__box">
+              <div class="Boxes__RightSide">
+                <div class="Boxes__title">Total Customer</div>
+                <div class="Boxes__number">11,086</div>
+                <div class="Boxes__icon">
+                  <div class="Boxes__iconDown">
+                  <ion-icon name="arrow-down-outline"></ion-icon>
+                  </div>
+                  <span class="Boxes__text">Down From 1 weeks ago</span>
+                </div>
+              </div>
+              <div class="Boxes__iconCustomer">
+              <ion-icon name="happy-outline"></ion-icon>
+              </div>
+            </div>
+          </div>
+        </section>
