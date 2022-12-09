e72a64d235376ab3318c8b3be08d188d0f42699d
raynaldysuchiady <rsuchiady@gmail.com>
2022-12-09 13:53:54 +0800

    Menambahkan halaman updatesupply, dan sidebar pada update dan create supply

---
+++ resources/views/supply/update.blade.php
@@ -0,0 +1,42 @@
+@extends('components.main')
+@section('content')
+    @include('components.sidebar')
+<div class="UpdateSupply">
+    <div class="UpdateSupply__container">
+        <div class="UpdateSupply__header">Supply Update</div>
+        <form class="UpdateSupply__form" action="{{url('/supply')}}" method="post">
+            @csrf
+            <div class="UpdateSupply__form-row">
+                <div class="UpdateSupply-form__input-data">
+                    <input class="UpdateSupply__data" type="text" name="nama"required>
+                    <div class="UpdateSupply--underline"></div>
+                    <label for="">Product Name</label>
+                </div>
+            </div>
+            <div class="UpdateSupply__form-row">
+                <div class="UpdateSupply-form__input-data">
+                    <input class="Supply-data" type="text" name="stock" required>
+                    <div class="UpdateSupply--underline"></div>
+                    <label for="">Stock Available</label>
+                </div>
+            </div>
+            <div class="UpdateSupply__form-row">
+                <div class="UpdateSupply-form__input-data">
+                    <input class="Supply-data" type="text" name="harga" required>
+                    <div class="UpdateSupply--underline"></div>
+                    <label for="">Price/each</label>
+                </div>
+            </div>
+            <br>
+            <br>
+            <div class="UpdateSupply__form-row UpdateSupply__submit-btn" id="UpdateSupply__submit-btn">
+                <div class="UpdateSupply-form__input-data">
+                <div class="UpdateSupplyinner"></div>
+                <input type="submit" value="submit">
+                </div>
+            </div>
+            
+        </form>
+    </div>
+</div>
+@endsection
